1db1647f4dd0d0a5678bcee6a176eabdc711176f
excalategit <oloruntoba@excalate.pl>
2025-05-08 13:00:53 +0200

    Code edited and formatted.

--- amazon/amazon sales report.py
+++ amazon/amazon sales report.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# The design here (based on best practice) is to load data to a staging table, transform and export dimension table data to their tables, create surrogate keys on those tables and import the surrogate keys back to the staging table. Afterwards transform and export fact table data together with all surrogate keys to the fact table. 
-# 
-# Note that this design sometimes require some transformation to be done on the data in staging before exporting to dimension tables. This is because at the step where surrogate keys are loaded back to staging, the action is based on a comparison between staging and dimension table data which will fail if one table is transformed and the other not.
-
-# In[1]:
-
+# The design here (based on best practice) is to load data to a staging table, transform
+# and export dimension table data to their tables, create surrogate keys on those tables and
+# import the surrogate keys back to the staging table.
+# Afterward transform and export fact table data together with all surrogate keys to the
+# fact table.
+# Note that this design sometimes require some transformation to be done on the data in staging
+# before exporting to dimension tables. This is because at the step where surrogate keys are
+# loaded back to staging, the action is based on a comparison between staging and dimension
+# table data which will fail if one table is transformed and the other not.
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -14,37 +17,28 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-
-# In[2]:
-
-
 load_dotenv()
 
-
 # Creating the fact and dimension tables where the transformed data will be loaded to.
-# 
-# Here the connection is defined with an additional specification for the schema since here the tables are not created on the default public schema.
-
-# In[36]:
-
+# Here the connection is defined with an additional specification for the schema since here
+# the tables are not created on the default public schema.
 
 connection = None
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
-    
+
 try:
-    
+
     with psycopg2.connect(
-        host = 'localhost',
-        dbname = 'Destination',
-        user = db_user,
-        password = db_password,
-        port = 5432,
-        options = '-c search_path=amazon') as connection:
+            host='localhost',
+            dbname='Destination',
+            user=db_user,
+            password=db_password,
+            port=5432,
+            options='-c search_path=amazon') as connection:
         # This is how to indicate the schema of interest
-    
+
         with connection.cursor() as cursor:
-            
             create_dim_product = '''
             CREATE TABLE IF NOT EXISTS dim_product (
             product_key SERIAL PRIMARY KEY,
@@ -57,18 +51,18 @@ try:
             rating TEXT,
             rating_count TEXT
             )'''
-            
-            cursor.execute(create_dim_product)          
-            
+
+            cursor.execute(create_dim_product)
+
             create_dim_user = '''
             CREATE TABLE IF NOT EXISTS dim_user (
             user_key SERIAL PRIMARY KEY,
             user_id TEXT,
             user_name TEXT
             )'''
-            
+
             cursor.execute(create_dim_user)
-            
+
             create_dim_review = '''
             CREATE TABLE IF NOT EXISTS dim_review (
             review_key SERIAL PRIMARY KEY,
@@ -76,9 +70,9 @@ try:
             review_title TEXT,
             review_content TEXT
             )'''
-            
+
             cursor.execute(create_dim_review)
-            
+
             create_fact_sale = '''
             CREATE TABLE IF NOT EXISTS fact_sale (
             sale_key SERIAL PRIMARY KEY,
@@ -89,18 +83,15 @@ try:
             "discounted_price (PLN)" FLOAT,
             discount_percentage TEXT
             )'''
-            
-            cursor.execute(create_fact_table)
-            
+
+            cursor.execute(create_fact_sale)
+
 except Exception as error:
     print(error)
-    
+
 finally:
     if connection is not None:
         connection.close()
-
-
-# In[40]:
 
 
 # Defining the function that will perform the INSERT action when called by the ETL stages.
@@ -109,17 +100,16 @@ def insert(insert_query, dataset):
     connection = None
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASSWORD')
-    
+
     try:
         with psycopg2.connect(
-            host = 'localhost',
-            dbname = 'Destination',
-            user = db_user,
-            password = db_password,
-            port = 5432) as connection:
+                host='localhost',
+                dbname='Destination',
+                user=db_user,
+                password=db_password,
+                port=5432) as connection:
 
             with connection.cursor() as cursor:
-
                 psycopg2.extras.execute_batch(cursor, insert_query, dataset)
 
     except Exception as error:
@@ -128,54 +118,54 @@ def insert(insert_query, dataset):
     finally:
         if connection is not None:
             connection.close()
-            
+
+
 # Defining the function that extracts and transforms to staging
 
 def extract_transform():
     connection = None
-    
+
     try:
-        engine2 = create_engine('postgresql:///Destination')
-        
-        source_table = pd.read_excel('amazon.xlsx') 
+        engine = create_engine('postgresql:///Destination')
+
+        source_table = pd.read_excel('amazon.xlsx')
         source_table.to_sql('stg_amazon_sales_report', engine, index=False, if_exists='replace')
-    
+
         # Addition of surrogate key columns to staging
         db_user = os.getenv('DB_USER')
         db_password = os.getenv('DB_PASS')
-        
+
         with psycopg2.connect(
-            host = 'localhost',
-            dbname = 'Destination',
-            user = db_user,
-            password = db_password,
-            port = 5432) as connection:
+                host='localhost',
+                dbname='Destination',
+                user=db_user,
+                password=db_password,
+                port=5432) as connection:
 
             with connection.cursor() as cursor:
-                
                 staging_update = '''ALTER TABLE stg_amazon_sales_report
                 ADD COLUMN product_key INT,
                 ADD COLUMN user_key INT,
                 ADD COLUMN review_key INT
                 '''
-                
+
                 cursor.execute(staging_update)
-                
+
                 return print('Extraction to staging completed')
-    
+
     except Exception as error:
         print(error)
-        
+
     finally:
         if connection is not None:
             connection.close()
 
+
 # Defining the function that loads the transformed data to the dimension tables
 
 def load_dim_product():
-
     engine = create_engine('postgresql:///Destination')
-    
+
     dp = pd.read_sql('stg_amazon_sales_report', engine)
     product = dp[['product_id', 'product_name', 'category', 'about_product', 'img_link', 'product_link',
                   'rating', 'rating_count']].copy()
@@ -204,14 +194,14 @@ def load_dim_product():
     %(rating)s,
     %(rating_count)s
     )'''
-    
+
     insert(insert_query, product)
     return print('dim_product loaded successfully')
 
+
 def load_dim_user():
-    
     engine = create_engine('postgresql:///Destination')
-    
+
     du = pd.read_sql('stg_amazon_sales_report', engine)
     user = du[['user_id', 'user_name']].copy()
     user = user.drop_duplicates()
@@ -229,8 +219,8 @@ def load_dim_user():
     insert(insert_query, user)
     return print('dim_user loaded successfully')
 
+
 def load_dim_review():
-    
     engine = create_engine('postgresql:///Destination')
 
     drr = pd.read_sql('stg_amazon_sales_report', engine)
@@ -252,56 +242,56 @@ def load_dim_review():
     insert(insert_query, review)
     return print('dim_review loaded successfully')
 
+
 # Defining the function that loads the surrogate keys to staging
 
 def load_surr_keys():
     connection = None
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASSWORD')
-        
+
     try:
-        
+
         with psycopg2.connect(
-            host = 'localhost',
-            dbname = 'Destination',
-            user = db_user,
-            password = db_password,
-            port = 5432) as connection:
+                host='localhost',
+                dbname='Destination',
+                user=db_user,
+                password=db_password,
+                port=5432) as connection:
 
             with connection.cursor() as cursor:
-                
                 product_key = '''UPDATE stg_amazon_sales_report AS s SET product_key = p.product_key 
                 FROM amazon.dim_product AS p WHERE s.product_id = p.product_id AND
                 s.product_name = p.product_name'''
                 cursor.execute(product_key)
-                
+
                 user_key = '''UPDATE stg_amazon_sales_report AS s SET user_key = u.user_key 
                 FROM amazon.dim_user AS u WHERE s.user_id = u.user_id AND
                 s.user_name = u.user_name'''
                 cursor.execute(user_key)
-                
+
                 review_key = '''UPDATE stg_amazon_sales_report AS s SET review_key = rr.review_key 
                 FROM amazon.dim_review AS rr WHERE s.review_id = rr.review_id AND
                 s.review_title = rr.review_title'''
                 cursor.execute(review_key)
-                
+
                 return print('staging updated with surr. keys successfully')
-    
+
     except Exception as error:
         print(error)
-        
+
     finally:
         if connection is not None:
             connection.close()
-            
+
+
 # Finally, defining the function that transforms and loads the fact table together with the surrogate keys
-            
+
 def transform_load_fact_table():
-    
     engine = create_engine('postgresql:///Destination')
-    
+
     dg = pd.read_sql('stg_amazon_sales_report', engine)
-    fact = dg[['discounted_price', 'actual_price', 'discount_percentage', 
+    fact = dg[['discounted_price', 'actual_price', 'discount_percentage',
                'product_key', 'user_key', 'review_key']].copy()
 
     fact['discounted_price'] = fact['discounted_price'].str.replace('₹', '')
@@ -331,44 +321,16 @@ def transform_load_fact_table():
     return print('fact_table loaded successfully')
 
 
-# In[38]:
-
-
 extract_transform()
-
-
-# In[41]:
-
 
 load_dim_product()
 
-
-# In[42]:
-
-
 load_dim_user()
-
-
-# In[43]:
-
 
 load_dim_review()
 
-
-# In[44]:
-
-
 load_surr_keys()
 
-
-# In[45]:
-
-
 transform_load_fact_table()
-
-
-# In[ ]:
-
-
 
 
