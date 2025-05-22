90b0e06d2dd5d4af600c7def5b9b2a3668f4f40f
excalategit <oloruntoba@excalate.pl>
2025-05-22 12:35:55 +0200

    Added row count indicator in readiness for data completeness tests.

--- Incremental Load/ebay incremental load.py
+++ Incremental Load/ebay incremental load.py
@@ -11,10 +11,11 @@ load_dotenv()
 # Defining the function that will perform the INSERT action when called by the ETL stages.
 
 
-def insert(insert_query, dataset):
+def insert(insert_query, dataset, table_name):
     connection = None
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASSWORD')
+    loaded_rows = 0
 
     try:
         with psycopg2.connect(
@@ -26,9 +27,11 @@ def insert(insert_query, dataset):
 
             with connection.cursor() as cursor:
                 psycopg2.extras.execute_batch(cursor, insert_query, dataset)
+                print(f'Rows {loaded_rows} to {len(dataset)} loaded successfully for {table_name}')
+                loaded_rows += len(dataset)
 
     except Exception as error:
-        print(error)
+        print(f'Loading failed for {table_name}: {error}')
 
     finally:
         if connection is not None:
@@ -38,7 +41,6 @@ def insert(insert_query, dataset):
 # Defining the function that extracts and transforms source data to staging.
 
 def extract_transform():
-
     try:
         engine = create_engine('postgresql:///Destination')
 
@@ -57,120 +59,138 @@ def extract_transform():
         # Best practice is to add a created date column to staging for audit purposes.
         source_table.to_sql('stg_product_review', engine, index=False, if_exists='append')
 
-        return print('Extraction to staging completed')
+        return print('Extraction to staging completed.')
 
     except Exception as error:
-        print(error)
+        print(f'Extraction to staging failed: {error}')
 
 
 # Defining the function that loads the transformed data to the dimension tables.
 
 def load_dim_product():
-    engine = create_engine('postgresql:///Destination')
+    table_name = 'dim_product'
 
-    dp = pd.read_sql('stg_product_review', engine)
-    product = dp[['product_id', 'product_name', 'category', 'about_product', 'img_link', 'product_link',
-                  'rating', 'rating_count', 'created_date']].copy()
+    try:
+        engine = create_engine('postgresql:///Destination')
 
-    product['created_date'] = pd.to_datetime(product['created_date']).dt.date
-    product = product[product['created_date'] == datetime.today().date()]
-    # This fetches only today's product data from staging.
-    product['rating_count'] = product['rating_count'].fillna(1)
-    product = product.drop_duplicates(subset=['product_id', 'product_name'], keep='first')
-    product = product.to_dict('records')
+        dp = pd.read_sql('stg_product_review', engine)
+        product = dp[['product_id', 'product_name', 'category', 'about_product', 'img_link', 'product_link',
+                      'rating', 'rating_count', 'created_date']].copy()
 
-    insert_query = '''INSERT into ebay.dim_product (
-    product_id,
-    product_name,
-    category,
-    about_product,
-    img_link,
-    product_link,
-    rating,
-    rating_count
-    ) 
-    VALUES (
-    %(product_id)s,
-    %(product_name)s,
-    %(category)s,
-    %(about_product)s,
-    %(img_link)s,
-    %(product_link)s,
-    %(rating)s,
-    %(rating_count)s
-    )
-    ON CONFLICT (product_id)
-    DO UPDATE SET
-    product_name = excluded.product_name,
-    category = excluded.category,
-    about_product = excluded.about_product,
-    img_link = excluded.img_link,
-    product_link = excluded.product_link,
-    rating = excluded.rating,
-    rating_count = excluded.rating_count
-    '''
+        product['created_date'] = pd.to_datetime(product['created_date']).dt.date
+        product = product[product['created_date'] == datetime.today().date()]
+        # This fetches only today's product data from staging.
+        product['rating_count'] = product['rating_count'].fillna(1)
+        product = product.drop_duplicates(subset=['product_id', 'product_name'], keep='first')
+        product = product.to_dict('records')
 
-    insert(insert_query, product)
-    return print('dim_product loaded successfully')
+        insert_query = '''INSERT into ebay.dim_product (
+        product_id,
+        product_name,
+        category,
+        about_product,
+        img_link,
+        product_link,
+        rating,
+        rating_count
+        ) 
+        VALUES (
+        %(product_id)s,
+        %(product_name)s,
+        %(category)s,
+        %(about_product)s,
+        %(img_link)s,
+        %(product_link)s,
+        %(rating)s,
+        %(rating_count)s
+        )
+        ON CONFLICT (product_id)
+        DO UPDATE SET
+        product_name = excluded.product_name,
+        category = excluded.category,
+        about_product = excluded.about_product,
+        img_link = excluded.img_link,
+        product_link = excluded.product_link,
+        rating = excluded.rating,
+        rating_count = excluded.rating_count
+        '''
+
+        insert(insert_query, product, table_name)
+        return None
+
+    except Exception as error:
+        print(f'Potential issue with transformation step: {error}')
 
 
 def load_dim_user():
-    engine = create_engine('postgresql:///Destination')
+    table_name = 'dim_user'
 
-    du = pd.read_sql('stg_product_review', engine)
-    user = du[['user_id', 'user_name', 'created_date']].copy()
+    try:
+        engine = create_engine('postgresql:///Destination')
 
-    user['created_date'] = pd.to_datetime(user['created_date']).dt.date
-    user = user[user['created_date'] == datetime.today().date()]
-    # This fetches only today's user data from staging.
-    user = user.drop_duplicates()
-    user = user.to_dict('records')
+        du = pd.read_sql('stg_product_review', engine)
+        user = du[['user_id', 'user_name', 'created_date']].copy()
 
-    insert_query = '''INSERT into ebay.dim_user (
-    user_id,
-    user_name
-    ) 
-    VALUES (
-    %(user_id)s,
-    %(user_name)s
-    )
-    ON CONFLICT (user_id)
-    DO UPDATE SET
-    user_name = excluded.user_name
-    '''
+        user['created_date'] = pd.to_datetime(user['created_date']).dt.date
+        user = user[user['created_date'] == datetime.today().date()]
+        # This fetches only today's user data from staging.
+        user = user.drop_duplicates()
+        user = user.to_dict('records')
 
-    insert(insert_query, user)
-    return print('dim_user loaded successfully')
+        insert_query = '''INSERT into ebay.dim_user (
+        user_id,
+        user_name
+        ) 
+        VALUES (
+        %(user_id)s,
+        %(user_name)s
+        )
+        ON CONFLICT (user_id)
+        DO UPDATE SET
+        user_name = excluded.user_name
+        '''
+
+        insert(insert_query, user, table_name)
+        return None
+
+    except Exception as error:
+        print(f'Potential issue with transformation step: {error}')
 
 
 def load_dim_review():
-    engine = create_engine('postgresql:///Destination')
+    table_name = 'dim_review'
 
-    drr = pd.read_sql('stg_product_review', engine)
-    review = drr[['review_id', 'review_title', 'created_date']].copy()
+    try:
+        engine = create_engine('postgresql:///Destination')
 
-    review['created_date'] = pd.to_datetime(review['created_date']).dt.date
-    review = review[review['created_date'] == datetime.today().date()]
-    # This fetches only today's review data from staging.
-    review = review.rename(columns={'review_title': 'review_content'})
-    review = review.drop_duplicates(subset=['review_id'], keep='first')
-    review = review.to_dict('records')
+        drr = pd.read_sql('stg_product_review', engine)
+        review = drr[['review_id', 'review_title', 'created_date']].copy()
 
-    insert_query = '''INSERT into ebay.dim_review (
-    review_id,
-    review_content
-    )
-    VALUES (
-    %(review_id)s,
-    %(review_content)s
-    )
-    ON CONFLICT (review_id)
-    DO UPDATE SET
-    review_content = excluded.review_content
-    '''
+        review['created_date'] = pd.to_datetime(review['created_date']).dt.date
+        review = review[review['created_date'] == datetime.today().date()]
+        # This fetches only today's review data from staging.
+        review = review.rename(columns={'review_title': 'review_content'})
+        review = review.drop_duplicates(subset=['review_id'], keep='first')
+        review = review.to_dict('records')
 
-    insert(insert_query, review)
-    return print('dim_review loaded successfully')
+        insert_query = '''INSERT into ebay.dim_review (
+        review_id,
+        review_content
+        )
+        VALUES (
+        %(review_id)s,
+        %(review_content)s
+        )
+        ON CONFLICT (review_id)
+        DO UPDATE SET
+        review_content = excluded.review_content
+        '''
+
+        insert(insert_query, review, table_name)
+        return None
+
+    except Exception as error:
+        print(f'Potential issue with transformation step: {error}')
 
 
 # Defining the function that fetches and loads surrogate keys to their respective target tables.
@@ -222,10 +242,10 @@ def load_surrogate_keys():
                 WHERE r.review_id = sa.review_id'''
                 cursor.execute(load_user_review)
 
-                return print('All target tables updated with surrogate keys successfully')
+                return print('All target tables updated with surrogate keys successfully.')
 
     except Exception as error:
-        print(error)
+        print(f'Loading surrogate keys failed: {error}')
 
     finally:
         if connection is not None:
@@ -236,37 +256,43 @@ def load_surrogate_keys():
 # all surrogate keys.
 
 def transform_load_fact_table():
-    engine = create_engine('postgresql:///Destination')
+    table_name = 'fact_table'
 
-    dg = pd.read_sql('stg_product_review', engine)
-    fact = dg[['discounted_price', 'actual_price', 'discount_percentage',
-               'product_key', 'created_date']].copy()
+    try:
+        engine = create_engine('postgresql:///Destination')
 
-    fact['created_date'] = pd.to_datetime(fact['created_date']).dt.date
-    fact = fact[fact['created_date'] == datetime.today().date()]
-    # This fetches only today's fact data from staging.
-    fact['discounted_price'] = fact['discounted_price'].str.replace('₹', '')
-    fact['discounted_price'] = fact['discounted_price'].str.replace(',', '').astype(float)
-    fact['actual_price'] = fact['actual_price'].str.replace('₹', '')
-    fact['actual_price'] = fact['actual_price'].str.replace(',', '').astype(float)
-    fact = fact.drop_duplicates(subset=['product_key'], keep='first')
-    fact = fact.to_dict('records')
+        dg = pd.read_sql('stg_product_review', engine)
+        fact = dg[['discounted_price', 'actual_price', 'discount_percentage',
+                   'product_key', 'created_date']].copy()
 
-    insert_query = '''INSERT into ebay.fact_price (
-    "actual_price (PLN)",
-    "discounted_price (PLN)",
-    discount_percentage,
-    product_key
-    ) 
-    VALUES (
-    %(actual_price)s,
-    %(discounted_price)s,
-    %(discount_percentage)s,
-    %(product_key)s
-    )'''
+        fact['created_date'] = pd.to_datetime(fact['created_date']).dt.date
+        fact = fact[fact['created_date'] == datetime.today().date()]
+        # This fetches only today's fact data from staging.
+        fact['discounted_price'] = fact['discounted_price'].str.replace('₹', '')
+        fact['discounted_price'] = fact['discounted_price'].str.replace(',', '').astype(float)
+        fact['actual_price'] = fact['actual_price'].str.replace('₹', '')
+        fact['actual_price'] = fact['actual_price'].str.replace(',', '').astype(float)
+        fact = fact.drop_duplicates(subset=['product_key'], keep='first')
+        fact = fact.to_dict('records')
 
-    insert(insert_query, fact)
-    return print('fact_table loaded successfully')
+        insert_query = '''INSERT into ebay.fact_price (
+        "actual_price (PLN)",
+        "discounted_price (PLN)",
+        discount_percentage,
+        product_key
+        ) 
+        VALUES (
+        %(actual_price)s,
+        %(discounted_price)s,
+        %(discount_percentage)s,
+        %(product_key)s
+        )'''
+
+        insert(insert_query, fact, table_name)
+        return None
+
+    except Exception as error:
+        print(f'Potential issue with transformation step: {error}')
 
 
 extract_transform()
