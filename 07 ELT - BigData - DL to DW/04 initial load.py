87a6bb6d615eda6cdfa343134a1b137edbb293a2
excalategit <oloruntoba@excalate.pl>
2025-11-19 12:36:07 +0100

    Using more descriptive comments.

--- 07 ELT - BigData - DL to DW/04 initial load.py
+++ 07 ELT - BigData - DL to DW/04 initial load.py
@@ -6,27 +6,81 @@ from time import time
 
 client = bigquery.Client()
 
-# Loading raw data blob from GCS bucket to BigQuery staging
-destination_table = 'bigdata_load.stg_bq_raw'
+# Fetching a raw data blob from GCS bucket and loading to BigQuery staging
+# This requires a different syntax from the usual read_sql, read_csv etc.
+uri = 'gs://my-dw-bucket-01/bq_source_data_01'
+destination_table = 'bigdata_load.bq_raw_staging'
 
 job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV, skip_leading_rows=1, autodetect=True
 )
 
-uri = 'gs://my-dw-bucket-01/bq_source_data_01'
-
-before_count = client.get_table(destination_table).num_rows
+before_load_count = client.get_table(destination_table).num_rows
 
 load_job = client.load_table_from_uri(uri, destination_table, job_config=job_config)
 load_job.result()
 
-after_count = client.get_table(destination_table).num_rows
-inserted_count = after_count - before_count
+after_load_count = client.get_table(destination_table).num_rows
+inserted_count = after_load_count - before_load_count
 
 print(f'Loaded {inserted_count} rows')
 
 
-# Creating the dim, fact, and audit tables
+# From staging, the raw data is extracted and transformed to a second staging table containing cleaned data
+def extract_transform():
+    try:
+        ds = read_gbq('bigdata_load.bq_raw_staging', 'my-dw-project-01')
+        source_table = ds.copy()
+
+        source_table['user_id'] = source_table['user_id'].astype(str)
+        source_table['user_name'] = source_table['user_name'].astype(str)
+        source_table['review_id'] = source_table['review_id'].astype(str)
+        source_table['review_title'] = source_table['review_title'].astype(str)
+
+        source_table['user_id'] = source_table['user_id'].str.split(',')
+        source_table['user_name'] = source_table['user_name'].str.split(',')
+        source_table['review_id'] = source_table['review_id'].str.split(',')
+        source_table['review_title'] = source_table['review_title'].str.split(',')
+        source_table = source_table.explode(['user_id', 'user_name', 'review_id', 'review_title'])
+
+        source_table['rating_count'] = source_table['rating_count'].fillna(1)
+
+        source_table['discounted_price'] = source_table['discounted_price'].astype(str)
+        source_table['discounted_price'] = source_table['discounted_price'].str.replace('₹', '')
+        source_table['discounted_price'] = source_table['discounted_price'].str.replace(',', '').astype(float)
+        source_table['actual_price'] = source_table['actual_price'].astype(str)
+        source_table['actual_price'] = source_table['actual_price'].str.replace('₹', '')
+        source_table['actual_price'] = source_table['actual_price'].str.replace(',', '').astype(float)
+
+        # source_table = source_table.rename(columns={'review_title': 'review_content'})
+        # source_table = source_table.rename(columns={'discounted_price': 'discounted_price_pln'})
+        # source_table = source_table.rename(columns={'actual_price': 'actual_price_pln'})
+
+        source_table = source_table.drop_duplicates()
+
+        # Finally, the data is assigned a created date of 'today' for audit purposes before loading to staging
+        source_table['created_date'] = datetime.today().date()
+
+        to_gbq(source_table, 'bigdata_load.bq_clean_staging',
+               project_id='my-dw-project-01', if_exists='fail')
+
+        # Addition of surrogate key columns to staging.
+        staging_update = '''
+        ALTER TABLE bigdata_load.bq_clean_staging
+        ADD COLUMN product_key STRING,
+        ADD COLUMN user_key STRING
+        '''
+
+        query_job = client.query(staging_update)
+        query_job.result()
+
+        return print('Extraction to staging completed')
+
+    except Exception as error:
+        print(f'Extraction to staging failed: {error}')
+
+
+# Creating the dimension, fact, and audit tables
 try:
     dim_product = '''
     CREATE TABLE IF NOT EXISTS bigdata_load.dim_product (
@@ -99,55 +153,8 @@ except Exception as error:
     print(error)
 
 
-def extract_transform():
-    try:
-        ds = read_gbq('bigdata_load.stg_bq_raw', 'my-dw-project-01')
-        source_table = ds.copy()
-
-        source_table['user_id'] = source_table['user_id'].astype(str)
-        source_table['user_name'] = source_table['user_name'].astype(str)
-        source_table['review_id'] = source_table['review_id'].astype(str)
-        source_table['review_title'] = source_table['review_title'].astype(str)
-
-        source_table['user_id'] = source_table['user_id'].str.split(',')
-        source_table['user_name'] = source_table['user_name'].str.split(',')
-        source_table['review_id'] = source_table['review_id'].str.split(',')
-        source_table['review_title'] = source_table['review_title'].str.split(',')
-        source_table = source_table.explode(['user_id', 'user_name', 'review_id', 'review_title'])
-
-        source_table['rating_count'] = source_table['rating_count'].fillna(1)
-
-        source_table['discounted_price'] = source_table['discounted_price'].astype(str)
-        source_table['discounted_price'] = source_table['discounted_price'].str.replace('₹', '')
-        source_table['discounted_price'] = source_table['discounted_price'].str.replace(',', '').astype(float)
-        source_table['actual_price'] = source_table['actual_price'].astype(str)
-        source_table['actual_price'] = source_table['actual_price'].str.replace('₹', '')
-        source_table['actual_price'] = source_table['actual_price'].str.replace(',', '').astype(float)
-
-        # source_table = source_table.rename(columns={'review_title': 'review_content'})
-        # source_table = source_table.rename(columns={'discounted_price': 'discounted_price_pln'})
-        # source_table = source_table.rename(columns={'actual_price': 'actual_price_pln'})
-        source_table['created_date'] = datetime.today().date()
-
-        to_gbq(source_table, 'bigdata_load.stg_bq_clean',
-               project_id='my-dw-project-01', if_exists='fail')
-
-        staging_update = '''
-        ALTER TABLE bigdata_load.stg_bq_clean
-        ADD COLUMN product_key STRING,
-        ADD COLUMN user_key STRING
-        '''
-
-        query_job = client.query(staging_update)
-        query_job.result()
-
-        return print('Extraction to staging completed')
-
-    except Exception as error:
-        print(f'Extraction to staging failed: {error}')
-
-
-def insert(project_id, dataset_id, dataframe, table_name, table_name_bq, column_name):
+# Defining the function that loads the data and updates the audit table when called.
+def loader(project_id, dataset_id, dataframe, table_name, table_name_bq, column_name):
     try:
         t1 = time()
         to_gbq(dataframe, f'{dataset_id}.{table_name}', project_id=project_id, if_exists='fail')
@@ -194,7 +201,7 @@ def load_dim_product():
                       'rating', 'rating_count']].copy()
         product = product.drop_duplicates(subset=['product_id', 'product_name'], keep='first')
 
-        insert(project_id, dataset_id, product, table_name, table_name_bq, column_name)
+        loader(project_id, dataset_id, product, table_name, table_name_bq, column_name)
 
     except Exception as error:
         print(f'Transformation stage failed for {table_name}: {error}')
@@ -212,7 +219,7 @@ def load_dim_user():
         user = du[['user_id', 'user_name']].copy()
         user = user.drop_duplicates()
 
-        insert(project_id, dataset_id, user, table_name, table_name_bq, column_name)
+        loader(project_id, dataset_id, user, table_name, table_name_bq, column_name)
 
     except Exception as error:
         print(f'Transformation stage failed for {table_name}: {error}')
@@ -230,7 +237,7 @@ def load_dim_review():
         review = dr[['review_id', 'review_title']].copy()
         review = review.drop_duplicates(subset=['review_id'], keep='first')
 
-        insert(project_id, dataset_id, review, table_name, table_name_bq, column_name)
+        loader(project_id, dataset_id, review, table_name, table_name_bq, column_name)
 
     except Exception as error:
         print(f'Transformation stage failed for {table_name}: {error}')
@@ -240,43 +247,31 @@ def load_surrogate_keys():
 
     try:
         # Loading surrogate keys from dimension tables to staging.
-        product_key = '''UPDATE bigdata_load.stg_bq_clean AS s SET product_key = p.product_key
+        product_key = '''UPDATE bigdata_load.bq_clean_staging AS s SET product_key = p.product_key
         FROM bigdata_load.dim_product AS p WHERE s.product_id = p.product_id AND
         s.product_name = p.product_name'''
         query_job = client.query(product_key)
         query_job.result()
 
-        user_key = '''UPDATE bigdata_load.stg_bq_clean AS s SET user_key = u.user_key
+        user_key = '''UPDATE bigdata_load.bq_clean_staging AS s SET user_key = u.user_key
         FROM bigdata_load.dim_user AS u WHERE s.user_id = u.user_id AND
         s.user_name = u.user_name'''
         query_job = client.query(user_key)
         query_job.result()
 
         # Loading dim_product table's surrogate keys from staging to dim_review.
-
         load_prod_review = '''
-                UPDATE bigdata_load.dim_review r SET product_key = s.product_key
-                FROM (
-                    SELECT * EXCEPT(row_num) FROM (
-                        SELECT *, ROW_NUMBER() OVER (PARTITION BY review_id ORDER BY created_date DESC) AS row_num
-                        FROM bigdata_load.stg_bq_clean
-                        ) WHERE row_num = 1
-                    ) AS s
+                UPDATE bigdata_load.dim_review AS r SET product_key = s.product_key
+                FROM bigdata_load.bq_clean_staging AS s
                 WHERE r.review_id = s.review_id
                 '''
         query_job = client.query(load_prod_review)
         query_job.result()
 
         # Loading dim_user table's surrogate keys from staging to dim_review.
-
         load_user_review = '''
                 UPDATE bigdata_load.dim_review AS r SET user_key = s.user_key
-                FROM (
-                    SELECT * EXCEPT(row_num) FROM (
-                        SELECT *, ROW_NUMBER() OVER (PARTITION BY review_id ORDER BY created_date DESC) AS row_num
-                        FROM bigdata_load.stg_bq_clean
-                        ) WHERE row_num = 1
-                    ) AS s
+                FROM bigdata_load.bq_clean_staging AS s
                 WHERE r.review_id = s.review_id
                 '''
         query_job = client.query(load_user_review)
@@ -302,7 +297,7 @@ def transform_load_fact_table():
         fact['discount_percentage'] = fact['discount_percentage'].astype(str)
         fact = fact.drop_duplicates(subset=['product_key'], keep='first')
 
-        insert(project_id, dataset_id, fact, table_name, table_name_bq, column_name)
+        loader(project_id, dataset_id, fact, table_name, table_name_bq, column_name)
 
     except Exception as error:
         print(f'Transformation stage failed for {table_name}: {error}')
@@ -310,11 +305,11 @@ def transform_load_fact_table():
 
 extract_transform()
 
-load_dim_review()
+load_dim_product()
 
 load_dim_user()
 
-load_dim_product()
+load_dim_review()
 
 load_surrogate_keys()
 
